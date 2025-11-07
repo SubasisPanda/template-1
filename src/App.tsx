9a397873149f89ebed0781d03eab14e05aaf8ee0
Subasis Panda <subasis85988@gmail.com>
2025-11-08 02:06:16 +0530

    routing Apps

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import './App.css'
 import Navbar from '@/components/navbar'
 import Landing from '@/pages/landing'
-import Pricing from "@/pages/pricing"; // <- create this
+import Pricing from "@/pages/pricing"; 
 import Features from "@/pages/features";
 import { Routes, Route } from "react-router-dom";
-
+import Footer from './components/footer';
+import Demo from '@/pages/demo';
 function App() {
 
   return (
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/features" element={<Features />} />
+        <Route path="/demo" element={<Demo />} />
+        
         <Route path="*" element={<div className="p-8 text-white">Not found</div>} />
       </Routes>
+      <Footer />
       
       
     </>
