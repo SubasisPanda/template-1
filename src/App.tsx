8a3249d804d03bf30451c0b1031bc37ecb4ba533
Subasis Panda <subasis85988@gmail.com>
2025-11-09 02:20:40 +0530

    rest collection

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import Features from "@/pages/features";
 import { Routes, Route } from "react-router-dom";
 import Footer from './components/footer';
 import Demo from '@/pages/demo';
+import { Terms } from '@/pages/terms';
+import { Blog } from '@/pages/blog';
+import { Blogs } from '@/pages/blogs';
+import  Contact  from '@/pages/contact';
 function App() {
 
   return (
@@ -17,6 +21,10 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/features" element={<Features />} />
         <Route path="/demo" element={<Demo />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
         
         <Route path="*" element={<div className="p-8 text-white">Not found</div>} />
       </Routes>
